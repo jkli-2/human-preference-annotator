0a27061e85431a6ab89eed23fabc4ea13d5dda3c
Junkai Li <junkai.li-2@student.uts.edu.au>
2025-08-26 11:32:29 +1000

    vid convert

---
+++ scripts/vid_conv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+INPUT_DIR="output_42_routes"
+OUTPUT_DIR="$INPUT_DIR/converted"
+
+mkdir -p "$OUTPUT_DIR"
+
+for f in $INPUT_DIR/*.mp4; do
+    [ -e "$f" ] || continue  # skip if no .mp4 files
+    filename=$(basename "$f")
+    ffmpeg -y -i "$f" \
+      -c:v libx264 -pix_fmt yuv420p -profile:v high -level 4.0 \
+      -movflags +faststart \
+      -preset veryfast -crf 20 \
+      -vsync cfr \
+      -an \
+      "$OUTPUT_DIR/$filename"
+done
+
